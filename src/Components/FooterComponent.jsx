e33323021eabecfda392c5c59d32b03a845ff00c
Hrithesh Reddy Buggana <bugganahritheshreddy@gmail.com>
2025-03-26 02:28:38 -0400

    first commit

---
+++ src/Components/FooterComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>All Rights Reserved by Hrithesh Reddy Buggana</span>
+        </footer>
+
+    </div>
+  )
+}
+
+export default FooterComponent
